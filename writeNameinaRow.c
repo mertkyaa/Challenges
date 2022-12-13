f71d1714c8c7646b56c3f5d1f68e5668567c2018
mertkyaa <119807112+mertkyaa@users.noreply.github.com>
2022-12-13 10:56:43 +0300

    Mert Kaya.c

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,22 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 int main(){
- int i,j,k,n; 
- printf("Enter the number of the row :");
- scanf("%d",&n);
- 
- for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
- }
-     printf("Akif")
- }
- 
-    
+	
+int i,j,k,n;
+
+printf("Enter the number of the row: ");
+scanf("%d",&n);
+printf("Enter the number of the roll: ");
+scanf("%d",&j);
+
+for(i=0;i<n;i++){
+	    for(j=0;j<n;j++){
+	    	printf("mert");
+	    	
+	    	
+			
+		}
+			printf("\n");
+	}
+return 0;
 }
