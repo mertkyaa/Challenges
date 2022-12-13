65a56467c2845141eac077e433812bcbb82fa6e7
mertkyaa <119807112+mertkyaa@users.noreply.github.com>
2022-12-13 10:05:02 +0300

    update name surname age.c

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,14 @@
 // your name surname and age from user
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  char name[20],surname[40];
+  int age;
+  printf("Enter your name: ");
+  scanf("%s",name);
+  printf("Enter your surname: ");
+  scanf("%s",surname);
+  printf("Enter your age: ");
+  scanf("%d\n",&age);
+  return  0;
